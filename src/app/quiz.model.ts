e7c54f0446d14cd06e4d6ac691a12ad68d0de540
Abstracted <antoine.luc@gmail.com>
2019-10-27 11:26:08 +0100

    Initial commit

---
+++ src/app/quiz.model.ts
@@ -0,0 +1,23 @@
+// src/quiz.model.ts
+export class Choice {
+constructor(public value: string, public correct?:boolean) {}
+
+}
+
+
+export class Question {
+    constructor(public label:string,public choises:Choice[]) {}
+}
+
+
+
+
+export class Quiz {
+    constructor(public label:string,public questions: Question []) {}
+}
+
+
+export class Answers {
+constructor (public values: Choice[]= []) {}
+
+}
